43160b4171e9a6773488e579154e815b9280f7de
mortiest-morty <mortyyy13@gmail.com>
2025-11-21 18:39:55 +0500

    Update game.sh

--- game.sh
+++ game.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
+main(){
+    echo -e "\nIt's a good game!\nTry to know what number computer could guess (number 1-100)\n"
+    echo -e "\nEnter a number, please : "
 
-echo -e "\nIt's a good game!\nTry to know what number computer could guess (number 1-100)\n"
-echo -e "\nEnter a number, please : "
-
-x=$(random_num)
-
-read num
-
-while [ $num -ne $x ]; do
-    if [ $num -lt $x ]; then
-        echo -e "Your number is less than guessed number\nEnter a number again, please : "
-    elif [ $num -gt $x ]; then
-        echo -e "Your number is more than guessed number\nEnter a number again, please : "
-    fi
+    x=$(random_num)
+    
     read num
-done
+
+    while [ $num -ne $x ]; do
+        if [ $num -lt $x ]; then
+            echo -e "Your number is less than guessed number\nEnter a number again, please : "
+        elif [ $num -gt $x ]; then
+            echo -e "Your number is more than guessed number\nEnter a number again, please : "
+        fi
+        read num
+    done
+}
